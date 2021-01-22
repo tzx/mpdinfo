4cfc9d7c4b19f9c590a0a521984b9f856138b74a
Timmy Xiao <timmyxiao1@gmail.com>
2021-01-22 16:52:24 -0500

    Add templating with arguments

--- server.go
+++ server.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"encoding/json"
+	"html/template"
+	"log"
 	"net/http"
+	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+type TemplateInfo struct {
+  MPDURL string
+  ServerURL string
+}
+
+var URLs TemplateInfo
+
+func getMPDInfo(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
 
   info, err := GetMPDInfo()
@@ -18,7 +28,24 @@ func handler(w http.ResponseWriter, r *http.Request) {
   json.NewEncoder(w).Encode(info)
 }
 
+func index(w http.ResponseWriter, r *http.Request) {
+  t, err := template.ParseFiles("./index.html")
+  if err != nil {
+    log.Fatal("Template Error:", err)
+  }
+  t.Execute(w, URLs)
+}
+
 func main() {
-  http.HandleFunc("/", handler)
+  http.HandleFunc("/status", getMPDInfo)
+  http.HandleFunc("/", index)
   http.ListenAndServe(":8080", nil)
+}
+
+func init() {
+  argsWithoutProg := os.Args[1:]
+  if len(argsWithoutProg) != 2 {
+    log.Fatal("Usage: mpdinfo mpdURL serverURL")
+  }
+  URLs = TemplateInfo{MPDURL: argsWithoutProg[0], ServerURL: argsWithoutProg[1]}
 }
